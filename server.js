6c93938c279ae6a63393a981df1442e3600f5260
OrehGDPS <OrehGDPS@gmail.com>
2026-05-02 00:29:20 +0300

    Refactor server setup and WebSocket connection handling

--- server.js
+++ server.js
@@ -3,33 +3,25 @@ const WebSocket = require("ws");
 
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end("Server online");
+const app = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("online");
 });
 
-const wss = new WebSocket.Server({ server });
-
-let clients = [];
+const wss = new WebSocket.Server({ server: app });
 
 wss.on("connection", (ws) => {
-    clients.push(ws);
-    console.log("Игрок подключился");
+  console.log("client connected");
 
-    ws.on("message", (message) => {
-        clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message.toString());
-            }
-        });
+  ws.on("message", (msg) => {
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(msg.toString());
+      }
     });
-
-    ws.on("close", () => {
-        clients = clients.filter(c => c !== ws);
-        console.log("Игрок вышел");
-    });
+  });
 });
 
-server.listen(PORT, () => {
-    console.log("Сервер запущен на порту " + PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Started on " + PORT);
 });
